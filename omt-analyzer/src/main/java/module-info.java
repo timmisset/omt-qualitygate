f4bfa2dfa64026ba5e6e9eac8e12b7837816b8ab
timmisset <timmisset@live.com>
2022-08-20 20:50:59 +0200

    Added java modules

---
+++ omt-analyzer/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module omt.analyzer {
+    requires omt.loader;
+    requires org.yaml.snakeyaml;
+    requires org.apache.commons.lang3;
+    exports com.misset.omt.qualitygate.analyzer.context;
+}
---
+++ omt-loader/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module omt.loader {
+    exports com.misset.omt.qualitygate.model;
+    exports com.misset.omt.qualitygate.model.parser;
+    exports com.misset.omt.qualitygate.model.maps.files;
+    exports com.misset.omt.qualitygate.model.scalars;
+    exports com.misset.omt.qualitygate.model.maps;
+    exports com.misset.omt.qualitygate.model.maps.variables;
+    exports com.misset.omt.qualitygate.model.maps.modelitem;
+    exports com.misset.omt.qualitygate.model.maps.modelitem.ontology;
+    requires org.yaml.snakeyaml;
+}
